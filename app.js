f1ef9d35e256cddd562b3e27ce92f081a5c0bdaa
udit <raiudit80@gmail.com>
2023-01-06 02:24:51 +0530

    cors error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express =require('express');
-// const cors = require('cors');
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 8000
 const mongoose = require('mongoose');
@@ -16,7 +17,7 @@ mongoose.connection.on('connected',()=>{
 mongoose.connection.on('error',(err)=>{
     console.log('error in database',err);
 })
-// app.use(cors);
+app.use(cors());
 //requiring schema
 require('./models/user');
 require('./models/post');
