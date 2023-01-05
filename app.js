a79586970714b7a85212544ff6b5c92f1a3149cc
udit <raiudit80@gmail.com>
2023-01-05 22:38:46 +0530

    final commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express =require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8000
 const mongoose = require('mongoose');
@@ -16,7 +16,7 @@ mongoose.connection.on('connected',()=>{
 mongoose.connection.on('error',(err)=>{
     console.log('error in database',err);
 })
-// app.use(cors);
+app.use(cors);
 //requiring schema
 require('./models/user');
 require('./models/post');
@@ -27,15 +27,6 @@ app.use(express.json());
 app.use(require('./routes/auth'));
 app.use(require('./routes/post'));
 app.use(require('./routes/user'));
-
-if(process.env.NODE_ENV=="production"){
-    
-    const path = require('path')
-    app.get("/",(req,res)=>{
-        app.use(express.static(path.resolve(__dirname,'client','build')))
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
 
 app.listen(PORT,()=>{
     console.log("server is running on ", PORT);
